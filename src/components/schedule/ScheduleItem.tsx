9e86eca192f2d8fd9dfd2546b868a796e0dfa1e4
dasom <ds92ko@gmail.com>
2025-12-26 03:22:22 +0900

    perf: ScheduleItem의 transform과 position 스타일 useMemo로 최적화
    - transform 문자열 변환을 useMemo로 메모이제이션
    - position 스타일 객체를 useMemo로 메모이제이션
    - 드래그 중 불필요한 문자열/객체 재생성 방지

--- src/components/schedule/ScheduleItem.tsx
+++ src/components/schedule/ScheduleItem.tsx
@@ -11,7 +11,7 @@ import {
 } from '@chakra-ui/react';
 import { useDraggable } from '@dnd-kit/core';
 import { CSS } from '@dnd-kit/utilities';
-import { ComponentProps, memo } from 'react';
+import { ComponentProps, memo, useMemo } from 'react';
 import { DAY_LABELS } from '../../constants/common';
 import { CellSize } from '../../constants/schedule';
 import { Schedule } from '../../types/schedule';
@@ -29,6 +29,20 @@ const ScheduleItem = memo(({ id, data, bg, onDeleteButtonClick }: ScheduleItemPr
   const topIndex = range[0] - 1;
   const size = range.length;
 
+  const transformString = useMemo(() => {
+    return transform ? CSS.Translate.toString(transform) : undefined;
+  }, [transform]);
+
+  const positionStyles = useMemo(
+    () => ({
+      left: `${120 + CellSize.WIDTH * leftIndex + 1}px`,
+      top: `${40 + topIndex * CellSize.HEIGHT + 1}px`,
+      width: `${CellSize.WIDTH - 1}px`,
+      height: `${CellSize.HEIGHT * size - 1}px`,
+    }),
+    [leftIndex, topIndex, size],
+  );
+
   const handleDelete = () =>
     onDeleteButtonClick?.({
       day,
@@ -40,16 +54,16 @@ const ScheduleItem = memo(({ id, data, bg, onDeleteButtonClick }: ScheduleItemPr
       <PopoverTrigger>
         <Box
           position="absolute"
-          left={`${120 + CellSize.WIDTH * leftIndex + 1}px`}
-          top={`${40 + (topIndex * CellSize.HEIGHT + 1)}px`}
-          width={CellSize.WIDTH - 1 + 'px'}
-          height={CellSize.HEIGHT * size - 1 + 'px'}
+          left={positionStyles.left}
+          top={positionStyles.top}
+          width={positionStyles.width}
+          height={positionStyles.height}
           bg={bg}
           p={1}
           boxSizing="border-box"
           cursor="pointer"
           ref={setNodeRef}
-          transform={CSS.Translate.toString(transform)}
+          transform={transformString}
           {...listeners}
           {...attributes}
         >
